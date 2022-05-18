53a62a5dc14f3761ebec07c7209d09b3ba3a1dbd
Ramadhani Musthofa <ramadhani.musthofa@gdn-commerce.com>
2022-05-18 12:50:00 +0700

    add hystrix circuit breaker

--- sample-eureka-client-1/src/main/java/com/example/sampleeurekaclient1/SampleEurekaClient1Application.java
+++ sample-eureka-client-1/src/main/java/com/example/sampleeurekaclient1/SampleEurekaClient1Application.java
@@ -1,13 +1,23 @@
 package com.example.sampleeurekaclient1;
 
 import com.netflix.discovery.EurekaClient;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+import com.netflix.hystrix.HystrixObservableCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.ReactiveHystrixCircuitBreakerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @EnableDiscoveryClient
@@ -20,10 +30,28 @@ public class SampleEurekaClient1Application {
 
 	@Autowired
 	private EurekaClient eurekaClient;
+	@Autowired
+	private ReactiveHystrixCircuitBreakerFactory circuitBreakerFactory;
 
 	@GetMapping("/")
 	public Mono<String> home() {
 		String instanceId = eurekaClient.getApplicationInfoManager().getInfo().getInstanceId();
-		return Mono.just(String.format("Hello from instance %s", instanceId));
+		int randomSeconds = ThreadLocalRandom.current().nextInt(1, 5);
+		return circuitBreakerFactory.create("home")
+				.run(Mono.delay(Duration.ofSeconds(randomSeconds)) // simulate delay
+								.then(Mono.just(String.format("Hello from instance %s", instanceId))),
+						throwable -> Mono.just("Fallback from circuit breaker with delay " + randomSeconds + " seconds"));
+	}
+}
+
+@Configuration
+class CircuitBreakerCustomizer {
+	@Bean
+	public Customizer<ReactiveHystrixCircuitBreakerFactory> defaultConfig() {
+		return factory -> factory.configureDefault(id -> HystrixObservableCommand.Setter
+				.withGroupKey(HystrixCommandGroupKey.Factory.asKey(id))
+				.andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
+						.withExecutionTimeoutInMilliseconds(2000)) // timeout 2 seconds
+		);
 	}
 }
